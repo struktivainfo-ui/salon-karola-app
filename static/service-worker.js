1396210e536d4598a0172af54caebc02860c442f
struktivainfo-ui <struktiva.info@gmail.com>
2026-03-19 17:25:57 +0100

    12345

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,21 @@
-self.addEventListener('install',()=>console.log('SW'));
+const CACHE_NAME = "salon-karola-v2-premium";
+const URLS_TO_CACHE = [
+  "/",
+  "/login",
+  "/static/style.css",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
