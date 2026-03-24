5756779becf3d9f45948476ee55118e7bdc21bb8
struktivainfo-ui <struktiva.info@gmail.com>
2026-03-24 17:36:36 +0100

    final cut

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def add_no_cache_headers(response):
         pass
     return response
 
-APP_VERSION = "3.5.11 Pro Menü Fix"
+APP_VERSION = "3.6.0 Pro Clean Left Menu"
 STAFF_OPTIONS = ["Alle", "Ute", "Jessi"]
 
 scheduler = BackgroundScheduler(timezone=os.getenv("APP_TIMEZONE", "Europe/Berlin"))
