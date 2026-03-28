55b134b973dfa2780c2e4ffe14b02378032bf105
struktivainfo-ui <struktiva.info@gmail.com>
2026-03-28 10:50:15 +0100

    kuss

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "salon-karola-v6-2-1-mobile-dashboard-clean";
+const CACHE_NAME = "salon-karola-v6-2-2-push-final";
 const STATIC_URLS = [
   "/static/style.css",
   "/static/icon-192.png",
